680bf915e5fca9f8ba1a5f59fc284f10dc734e59
Frederick Cheung <frederick.cheung@gmail.com>
2015-04-27 09:47:52 +0100

    add a refresh method

--- lib/active_support/cache/dalli_elasticache_store.rb
+++ lib/active_support/cache/dalli_elasticache_store.rb
@@ -6,8 +6,28 @@ module ActiveSupport
     class DalliElasticacheStore < DalliStore
       def initialize(*endpoint_and_options)
         endpoint, *options = endpoint_and_options
-        elasticache = Dalli::ElastiCache.new(endpoint)
-        super(elasticache.servers, options)
+        @elasticache = Dalli::ElastiCache.new(endpoint)
+
+        @pool_options = {}
+        if dalli_options = options.last && dalli_options.is_a?(Hash)
+          @pool_options[:size] = dalli_options[:pool_size] if dalli_options[:pool_size]
+          @pool_options[:timeout] = dalli_options[:pool_timeout] if dalli_options[:pool_timeout]
+        end
+
+        super(@elasticache.servers, options)
+      end
+
+      def refresh
+        old_version = @elasticache.version
+        @elasticache.refresh
+        if old_version < @elasticache.version
+          Rails.logger.info "Refreshing dalli-elasticache servers. New servers: #{@elasticache.servers}, version: #{@elasticache.version}"
+          if @pool_options.empty?
+            @data = Dalli::Client.new(@elasticache.servers, @options)
+          else
+            @data = ::ConnectionPool.new(@pool_options.dup) { Dalli::Client.new(@elasticache.servers, @options.merge(:threadsafe => false)) }
+          end
+        end
       end
     end
   end
